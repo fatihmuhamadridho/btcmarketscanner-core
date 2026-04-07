57edcb78d1308ed14a761dfb729661b29bdbd683
Fatih Muhamad Ridho <fatih.ridho@salt.co.id>
2026-04-07 12:38:52 +0700

    feat: 🎸 init project

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+declare function helloCore(): string;
+declare function formatText(symbol: string): string;
+
+export { formatText, helloCore };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export function helloCore() {
+  return "btcmarketscanner-core is working 🚀";
+}
+
+export function formatText(symbol: string) {
+  return `[core] scanning ${symbol}`;
+}
